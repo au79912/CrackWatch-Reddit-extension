49c0ea36f62e18947ddb5daeadb41259612756f0
ayihan <ayihanhaq@gmail.com>
2022-07-22 20:49:57 +0500

    READY!

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const crackBtn = document.getElementById('sred-btn')
 const fitBtn = document.getElementById("sfit-btn")
 const dodiBtn = document.getElementById("sdodi-btn")
 const logBtn = document.getElementById("log-btn")
+const logEl = document.getElementById("log-el")
 const inputEl = document.getElementById("input-el")
 const p1El = document.getElementById("p1-el")
 const p2El = document.getElementById("p2-el")
@@ -13,10 +14,9 @@ inputEl.addEventListener('keypress', function (e) {
 		redditSearch()
 	}
 })
-
 fitBtn.addEventListener('click', fitSearch)
-
 dodiBtn.addEventListener('click', dodiSearch)
+logBtn.addEventListener('click', renderlinks)
 
 function redditSearch() {
 	let redsearch = inputEl.value
@@ -39,6 +39,8 @@ function redditSearch() {
 				p2El.innerHTML = `<img src="found.png" alt="Results found">`
 			}
 		})
+
+		
 }
 
 function fitSearch() {
@@ -54,4 +56,12 @@ function dodiSearch() {
 	url = url.replace(/\s/g, "+")
 	// console.log(url)
 	window.open(url, '_blank').focus()
+}
+
+function renderlinks() {
+	let name = inputEl.value
+	name = name.replace(/\s/g, "+")
+	let link = `https://www.reddit.com/r/CrackWatch/search/?q=${name}%2020&restrict_sr=1&sr_nsfw=`
+	// print the link in logEl and open it in a new tab
+	logEl.innerHTML = `<a target="_blank" href="${link}">${link}</a>`
 }
