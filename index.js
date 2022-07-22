f353f0b7310572f9a36b5ef6ade15b7924c0bce9
ayihan <ayihanhaq@gmail.com>
2022-07-22 20:16:46 +0500

    added enter even listener to crackwatch search

--- index.js
+++ index.js
@@ -2,24 +2,35 @@ const crackBtn = document.getElementById('sred-btn')
 const fitBtn = document.getElementById("sfit-btn")
 const logBtn = document.getElementById("log-btn")
 const inputEl = document.getElementById("input-el")
-const pEl = document.getElementById("p2-el")
+const p1El = document.getElementById("p1-el")
+const p2El = document.getElementById("p2-el")
 
-crackBtn.addEventListener('click', function () {
+crackBtn.addEventListener('click', redditSearch)
+inputEl.addEventListener('keypress', function (e) {
+	if (e.keyCode === 13) {
+		redditSearch()
+	}
+})
+
+function redditSearch() {
 	let redsearch = inputEl.value
+	redsearch = redsearch.replace(/\s/g, "+")
 	fetch(`https://www.reddit.com/r/crackwatch/search.json?q=${redsearch}&restrict_sr=on&sort=relevance&t=all&limit=100`)
 		.then(function (response) {
 			return response.json()
 		})
 		.then(function (data) {
 			if (data.data.children.length === 0) {
-				console.log("No results found")
-				//insert NaN.png to pEl here
-				pEl.innerHTML = `<img src="NaN.png" alt="No results found">`
+				// console.log("No results found")
+				p1El.textContent = "No results found 😔"
+				p2El.innerHTML = `<img src="NaN.png" alt="No results found">`
 			}
-			else
-			{
-				console.log("Results found")
-				pEl.innerHTML = `<img src="found.png" alt="Results found">`
+
+			else {
+				// console.log("Results found")
+				p1El.textContent = "Results found 🏴‍☠️☠"
+				p2El.innerHTML = `<img src="found.png" alt="Results found">`
 			}
 		})
-})
+}
+
